fc92449ae2222bf2e4003836584366caa8119a64
Oleg Kreminsky <okreminsky@mementia.com>
2022-10-05 10:39:28 +0300

    Created Employee class.

--- Employee.php
+++ Employee.php
@@ -1,20 +1,38 @@
 <?php
 
-class Employee
-{
+class Employee {
     public $name;
     public $age;
     public $salary;
 
-    public function person($name, $age, $salary) {
-        return 'Person ' . $name . 'with ' . $age . 'has salary' . $salary;
+    public function getName($name) {
+        return 'ім`я працівника' . $name;
+    }
+
+    public function getAge($age) {
+        return 'Вік працівника' . $age;
+    }
+
+    public function getSalary($salary) {
+        return $salary;
+    }
+
+    public function checkAge($age) {
+           return $age > 18;
     }
 }
 
-$firstPerson = new Employee;
-$secondPerson = new Employee;
+$firstPerson = new Employee();
 
-echo $firstPerson->person('john', '25', '1000');
-echo $secondPerson->person('eric', '26', '2000');
-echo $firstPerson->salary + $secondPerson->salary;
-echo $firstPerson->age + $secondPerson->age;
+$firstPerson->getName('Oleg');
+$firstPerson->getAge('22');
+$firstPerson->getSalary('1000');
+
+$secondPerson = new Employee();
+
+$secondPerson->getName('Serge');
+$secondPerson->getAge('25');
+$secondPerson->getSalary('2000');
+
+print_r('Суму зарплат створених працівників ' . ($firstPerson->getSalary('1000') + $secondPerson->getSalary('2000')));
+
